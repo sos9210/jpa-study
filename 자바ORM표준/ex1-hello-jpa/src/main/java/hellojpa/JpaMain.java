2be6be426c150781c6d2417bb4dbf0eb1b0e23c7
sungsanghyun <sos9215@gmail.com>
2022-08-18 00:09:31 +0900

    실전예제 1 - 요구사항분석, 기본매핑

--- 자바ORM표준/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바ORM표준/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,12 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //EntityManagerFactory는 데이터베이스당 하나만 생성해서 애플리케이션 전체에서 공유한다
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        //EntityManager는 쓰레드간 공유해선 안된다.
         EntityManager em = emf.createEntityManager();
+/*
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
@@ -37,5 +40,6 @@ public class JpaMain {
         }
 
         emf.close();
+ */
     }
 }
