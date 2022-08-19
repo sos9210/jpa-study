e8d7e9b7bd2bd06e95e23cc1af6a68b2e95d331d
sungsanghyun <sos9210@naver.com>
2022-08-19 17:26:50 +0900

    다양한 연관관계 매핑

---
+++ 자바ORM표준/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    //중간테이블 (실무에서는 다대사 사용절대X)
+    @JoinTable(name = "CATEGORY_ITEM",
+                joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+                inverseJoinColumns = @JoinColumn(name = "ITEM_ID"))
+    private List<Item> items = new ArrayList<>();
+}
