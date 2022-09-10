66798127f6fa81ac7e4716e27ad99298fa5b9a80
sungsanghyun <sos9215@gmail.com>
2022-09-11 01:56:16 +0900

    스프링데이터JPA - 확장기능, 벌크수정, @EntityGraph, Hint & LOCK

--- 스프링데이터JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 스프링데이터JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -13,6 +13,8 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
     @Autowired TeamRepository teamRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     public void testMember() {
         Member member = new Member("memberA");
@@ -192,4 +198,85 @@ class MemberRepositoryTest {
         assertEquals(slice.hasNext(),true);         //다음페이지 존재여부
     }
 
+    @Test
+    public void bulkUpdate() {
+        //given
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",19));
+        memberRepository.save(new Member("member3",20));
+        memberRepository.save(new Member("member4",21));
+        memberRepository.save(new Member("member5",40));
+
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+        //        em.clear();
+
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+
+        System.out.println("member5 = " + member5);
+
+        //then
+        assertEquals(resultCount,3);
+    }
+
+    @Test
+    public void findMemberLazy() {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> members = memberRepository.findAll();
+        //List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+        //List<Member> members = memberRepository.findMemberEntityGraph();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("TeamClass = " + member.getTeam().getClass());
+            System.out.println("memberTeam = " + member.getTeam().getName());
+        }
+    }
+
+    @Test
+    public void queryHint() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername(member1.getUsername());
+        findMember.setUsername("member2");
+        em.flush();
+    }
+
+    @Test
+    public void lock(){
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername(member1.getUsername());
+    }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
