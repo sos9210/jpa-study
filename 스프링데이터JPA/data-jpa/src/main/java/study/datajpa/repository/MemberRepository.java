b503c474d71e78f3b60360742b338985f0bba681
sungsanghyun <sos9210@naver.com>
2022-09-11 01:56:16 +0900

    스프링데이터JPA - 확장기능, 벌크수정, @EntityGraph, Hint & LOCK

--- 스프링데이터JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 스프링데이터JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,17 +3,18 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member,Long>, MemberRepositoryCustom {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
 //    @Query(name = "Member.findByUsername")
@@ -37,4 +38,30 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query(value = "select m from Member m left join m.team t",countQuery = "select count(m) from Member m")
     Page<Member> findByAge(int age, Pageable pageable);
+
+    //변경쿼리임을 알려주는 애노테이션 (clearAutomatically = trye .. 이 옵션은 쿼리 실행후 영속성컨텍스트 초기화)
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age+1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"}) //페치조인
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //@EntityGraph("Member.all") //엔티티에 정의된 @NamedEntityGraph를 활용해서 사용하는 방법
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findEntityGraphByUsername(String username);
+
+    @QueryHints(@QueryHint(name = "org.hibernate.readOnly",value = "true"))
+    Member findReadOnlyByUsername(String username);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
 }
